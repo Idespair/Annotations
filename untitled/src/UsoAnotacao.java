214bd89fd83d4191980eceed8ec3fbe98ca8ce40
Idespair <kawanptbr@gmail.com>
2023-12-13 16:05:28 -0300

    Exercicio reflection

--- untitled/src/UsoAnotacao.java
+++ untitled/src/UsoAnotacao.java
@@ -2,10 +2,13 @@ import java.lang.annotation.Annotation;
 
 @Tabela(nome = "Tabela 1")
 public class UsoAnotacao {
-    static Annotation anno = Tabela.getClass().getAnnotation("Tabela 1");
-
     public static void main(String[] args) {
-        System.out.println(anno);
+        Class<UsoAnotacao> classe = UsoAnotacao.class;
+        Tabela tabela = classe.getAnnotation(Tabela.class);
+
+        String answer = tabela.nome();
+
+        System.out.println(answer);
     }
 }
 
